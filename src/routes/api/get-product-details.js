43be4b35a456fa08f23b4354e2e826c1b97d8c29
Owen Franssen <owen@studioforty9.com>
2022-07-05 15:25:28 +0100

    Organise storefront api calls

--- src/routes/api/get-product-details.js
+++ src/routes/api/get-product-details.js
@@ -1,76 +1,20 @@
 import { writable } from "svelte/store";
 import { postToShopify } from "./src/routes/api/utils/postToShopify";
 
-export const products = writable([]);
 export const productDetails = writable([]);
 
-export const getProducts = async () => {
-  try {
-    // @ts-ignore
-    const shopifyResponse = await postToShopify({
-      query: `{
-	     products(sortKey: TITLE, first: 100) {
-	      edges {
-	        node {
-	          id
-	          handle
-	          description
-	          title
-	          totalInventory
-	          productType
-	          variants(first: 5) {
-	            edges {
-	              node {
-	                id
-	                title
-	                quantityAvailable
-	                price
-	              }
-	            }
-	          }
-	          priceRange {
-	            maxVariantPrice {
-	              amount
-	              currencyCode
-	            }
-	            minVariantPrice {
-	              amount
-	              currencyCode
-	            }
-	          }
-	          images(first: 1) {
-	            edges {
-	              node {
-	                src
-	                altText
-	              }
-	            }
-	          }
-	        }
-	      }
-	    }
-    }
-	  `,
-    });
-    products.set(shopifyResponse.products.edges);
-    return shopifyResponse;
-  } catch (error) {
-    console.log(error);
-  }
-};
-// Get product details
 export const getProductDetails = async (handle) => {
   try {
     const shopifyResponse = await postToShopify({
       query: `
         query getProduct($handle: String!) {
-          productByHandle(handle: $handle) {
+          product(handle: $handle) {
             id
             handle
             description
             title
             totalInventory
-            variants(first: 5) {
+            variants(first: 20) {
               edges {
                 node {
                   id
@@ -93,7 +37,7 @@ export const getProductDetails = async (handle) => {
                 currencyCode
               }
             }
-            images(first: 1) {
+            images(first: 20) {
               edges {
                 node {
                   src
---
+++ src/routes/api/get-products.js
@@ -0,0 +1,59 @@
+import { writable } from "svelte/store";
+import { postToShopify } from "./src/routes/api/utils/postToShopify";
+
+export const products = writable([]);
+
+export const getProducts = async () => {
+  try {
+    // @ts-ignore
+    const shopifyResponse = await postToShopify({
+      query: `{
+	     products(sortKey: TITLE, first: 100) {
+	      edges {
+	        node {
+	          id
+	          handle
+	          description
+	          title
+	          totalInventory
+	          productType
+	          variants(first: 5) {
+	            edges {
+	              node {
+	                id
+	                title
+	                quantityAvailable
+	                price
+	              }
+	            }
+	          }
+	          priceRange {
+	            maxVariantPrice {
+	              amount
+	              currencyCode
+	            }
+	            minVariantPrice {
+	              amount
+	              currencyCode
+	            }
+	          }
+	          images(first: 1) {
+	            edges {
+	              node {
+	                src
+	                altText
+	              }
+	            }
+	          }
+	        }
+	      }
+	    }
+    }
+	  `,
+    });
+    products.set(shopifyResponse.products.edges);
+    return shopifyResponse;
+  } catch (error) {
+    console.log(error);
+  }
+};

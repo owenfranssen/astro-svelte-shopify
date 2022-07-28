d35fad93303af9a2fc4558a205ff396fa05924b8
Owen Franssen <owen@studioforty9.com>
2022-07-28 17:38:22 +0100

    Featured collections and collection graphql query

---
+++ src/routes/api/get-collection-details.js
@@ -0,0 +1,39 @@
+import {writable} from 'svelte/store';
+import {postToShopify} from './utils/postToShopify';
+
+export const collectionDetails = writable([]);
+
+export const getCollectionDetails = async (handle) => {
+	try {
+		const shopifyResponse = await postToShopify({
+			query: `
+				query GetCollection($handle: String!) {
+					collectionByHandle(handle: $handle) {
+						id,
+						handle,
+						title,
+						description,
+						descriptionHtml,
+						seo {
+							description
+							title
+						},
+						image {
+							id
+							altText
+							url
+						}
+					}
+				}
+      `,
+			variables: {
+				handle: handle,
+			},
+		});
+
+		collectionDetails.set(shopifyResponse.collectionByHandle);
+		return shopifyResponse.collectionByHandle;
+	} catch (error) {
+		console.log(error);
+	}
+};

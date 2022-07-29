298382fcd3ff1b09231e30255c043c7eebf5c0eb
Owen Franssen <owen@studioforty9.com>
2022-07-29 16:14:01 +0100

    Tidy up API call organisation

--- src/routes/api/get-products.js
+++ src/routes/api/get-products.js
@@ -3,7 +3,81 @@ import {postToShopify} from './utils/postToShopify';
 
 export const products = writable([]);
 
-export const getProducts = async () => {
+// Product in specified collection (by collection handle)
+export const getProductsInCollection = async (handle, limit = 10) => {
+	try {
+		// @ts-ignore
+		const shopifyResponse = await postToShopify({
+			query: `
+        query Collection($handle: String!, $limit: Int) {
+          collection(handle: $handle) {
+            id
+            handle
+            title
+            description
+            descriptionHtml
+            image {
+              id
+              url
+              altText
+              height
+              width
+            }
+            products(sortKey: TITLE, first: $limit) {
+              edges {
+                node {
+                  id
+                  handle
+                  description
+                  title
+                  totalInventory
+                  productType
+                  priceRange {
+                    maxVariantPrice {
+                      amount
+                      currencyCode
+                    }
+                    minVariantPrice {
+                      amount
+                      currencyCode
+                    }
+                  }
+                  compareAtPriceRange {
+                    maxVariantPrice {
+                      amount
+                      currencyCode
+                    }
+                    minVariantPrice {
+                      amount
+                      currencyCode
+                    }
+                  }
+                  featuredImage {
+                        src
+                        altText
+                        width
+                        height
+                  }
+                }
+              }
+            }
+          }
+        }
+      `,
+			variables: {
+				handle,
+        limit
+			},
+		});
+		products.set(shopifyResponse.collection);
+		return shopifyResponse.collection;
+	} catch (error) {
+		console.log('getProductsInCollection error: ', error);
+	}
+};
+
+// All products
+export const getAllProducts = async () => {
 	try {
 		// @ts-ignore
 		const shopifyResponse = await postToShopify({

5c85cd94a94d9c5bc8c59c806733327e249ecf4c
Owen Franssen <owenfranssen@gmail.com>
2022-09-06 16:20:56 +0100

    Working checkout button in mini cart

--- src/atoms/CheckoutButton.svelte
+++ src/atoms/CheckoutButton.svelte
@@ -2,6 +2,4 @@
 	import {checkoutLink} from '../assets/scripts/stores.js';
 </script>
 
-<a href={$checkoutLink} class="flex items-center justify-center rounded-md border border-transparent bg-indigo-600 px-6 py-3 text-base font-medium text-white shadow-sm hover:bg-indigo-700">Checkout</a>
-
-<!-- getCheckoutLink returns a promise and doesnt appear to update even though the console logs the correct URL... -->
+<a href={`${$checkoutLink.replaceAll('"', '')}`} class="flex items-center justify-center rounded-md border border-transparent bg-indigo-600 px-6 py-3 text-base font-medium text-white shadow-sm hover:bg-indigo-700">Checkout</a>

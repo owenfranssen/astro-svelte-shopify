b659aaa3e3b0c5dd4ba083c5be52492f8e3aa72c
Owen Franssen <owenfranssen@gmail.com>
2023-07-14 10:52:59 +0100

    Home page featured collections from json

---
+++ src/routes/api/get-collection-details.json.js
@@ -0,0 +1,19 @@
+import {writable} from 'svelte/store';
+import json from '../../config/collections.json';
+
+export const collectionDetails = writable([]);
+
+export const getCollectionDetails = (handle) => {
+	try {
+		const collections = json.collections;
+		for (const collection of collections) {
+			if (collection.handle == handle) {
+				return collection;
+			}
+		}
+
+		return false;
+	} catch (error) {
+		console.log('getCollectionDetails: ', error);
+	}
+};
